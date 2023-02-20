40696770d8ed7b47abf20535c51c6404e594b60b
Caiosanticchio <105894663+Caiosanticchio@users.noreply.github.com>
2023-02-20 14:40:26 -0300

    Delete javacript.js

--- javacript.js
+++
@@ -1,6 +0,0 @@
-const botaomenu = document.querySelector('.menu')
-const menu = document.querySelector('.menu-lateral')
-
-botaomenu.addEventListener('click', () =>{
-menu.classList.toggle('menu--ativo')
-   })
