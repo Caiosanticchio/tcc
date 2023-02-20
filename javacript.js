bf674a8f3e722a2f5f7706f094156493114c9fc5
Caiosanticchio <105894663+Caiosanticchio@users.noreply.github.com>
2023-02-20 13:40:59 -0300

    Add files via upload

---
+++ javacript.js
@@ -0,0 +1,6 @@
+const botaomenu = document.querySelector('.menu')
+const menu = document.querySelector('.menu-lateral')
+
+botaomenu.addEventListener('click', () =>{
+menu.classList.toggle('menu--ativo')
+   })
